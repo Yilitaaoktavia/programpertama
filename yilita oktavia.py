e39d8f42f77b40a3e4534b635bb074e36e5f73b7
Yilitaaoktavia <147121027+Yilitaaoktavia@users.noreply.github.com>
2023-10-06 11:00:07 +0700

    Add files via upload

---
+++ yilita oktavia.py
@@ -0,0 +1,6 @@
+print('ini program saya')
+print ('nama saya yilita')
+print ('alamat saya koba')
+print('hobi saya bermain bulu tangkis')
+print('saya berasal dari kelas ilkom')
+print('saya lahir di bangka tengah 25 oktober 2005')
